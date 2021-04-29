c1afcef81a804bed61f3b906c3a60b83129a7032
Simon Aronsson <simme@arcticbit.se>
2021-04-29 12:41:31 +0200

    fix: upgrade dependencies and fix example code

--- main.js
+++ main.js
@@ -1,31 +1,29 @@
-import http from "k6/http";
-import { check } from "k6";
-import cheerio from "cheerio";
-import xml2js from "xml2js";
+import http from 'k6/http';
+import { check } from 'k6';
+import cheerio from 'cheerio';
+import xml2js from 'xml2js';
 
-
-export default function() {
-
-  const res = http.get("https://loadimpact.com/");
+export default function () {
+  const res = http.get('https://loadimpact.com/');
 
   const $ = cheerio.load(res.body);
 
   const title = $('head title').text();
   check(title, {
-    "has correct title": () => title == 'Load Impact | Performance testing for DevOps teams'
+    'has correct title': () => title == 'Load Impact is now k6',
   });
-console.log(title);
+  console.log(title);
 
-
-  var xmlString = '<?xml version="1.0" ?>' +
-   	'<items xmlns="http://foo.com">' +
-   ' <item>Foo</item>' +
-   ' <item color="green">Bar</item>' +
-   '</items>'
+  var xmlString =
+    '<?xml version="1.0" ?>' +
+    '<items xmlns="http://foo.com">' +
+    ' <item>Foo</item>' +
+    ' <item color="green">Bar</item>' +
+    '</items>';
 
   xml2js.parseString(xmlString, function (err, result) {
-      console.log(JSON.stringify(err));
-      console.log(result);
-      console.log(JSON.stringify(result));
+    console.log(JSON.stringify(err));
+    console.log(result);
+    console.log(JSON.stringify(result));
   });
 }
