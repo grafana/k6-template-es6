b0d1c757b5981acba74d30a453831b46b94a4728
Simon Aronsson <simme@arcticbit.se>
2021-05-04 12:07:37 +0200

    Fix/remote module issue (#7)
    * fix: transpilation issue
    
    * fix: allow using remote modules as part of a webpack project

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,26 @@
- var path = require('path');
- var webpack = require('webpack');
- module.exports = {
-     mode: "production",
-     entry: './main.js',
-     output: {
-         path: path.resolve(__dirname, 'build'),
-         libraryTarget: "commonjs",
-         filename: 'app.bundle.js'
-     },
-     module: {
-         rules: [
-             {
-                 test: /\.js$/,
-                 loader: 'babel-loader',
-             }
-         ]
-     },
-     stats: {
-         colors: true
-     },
-     target: "web",
-         externals: /k6(\/.*)?/,
-     devtool: 'source-map'
- };
+var webpack = require('webpack');
+var path = require('path');
 
+module.exports = {
+  mode: 'production',
+  entry: './main.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    libraryTarget: 'commonjs',
+    filename: 'app.bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+  stats: {
+    colors: true,
+  },
+  target: 'web',
+  externals: /^(k6|https?\:\/\/)(\/.*)?/,
+  devtool: 'source-map',
+};
